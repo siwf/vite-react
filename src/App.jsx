de6e6931b2123194efb1e1bec82af09fb4d53e9d
siwenfeng <287235779@qq.com>
2022-07-31 22:00:49 +0800

    react 学习

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+/*
+ * @Description: 
+ * @Date: 2022-07-31 10:50:29
+ * @LastEditors: siwenfeng
+ * @LastEditTime: 2022-07-31 21:58:50
+ */
+import { Input, Table, Space, Popconfirm } from 'antd'
+import { AudioOutlined } from '@ant-design/icons'
+import axios from 'axios'
+import React, { useState } from 'react'
+
+import './App.css'
+const { Search } = Input
+
+class App extends React.Component {
+  state = {
+    list: [],
+    columns: [
+      {
+        title: '任务编号',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: '任务名称',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '任务描述',
+        dataIndex: 'des',
+        key: 'des',
+      },
+      {
+        title: '操作',
+        dataIndex: 'do',
+        key: 'do',
+        render: (text, record) => (
+          <Space size="middle">
+            <Popconfirm title="确定要删除吗?"
+              onConfirm={() => this.handleDelete(record.id)}>
+              <a href="#">删除</a>
+            </Popconfirm>
+          </Space>
+        ),
+      },
+    ]
+  }
+
+  // 搜索
+  onSearch = async (value) => {
+    const res = await axios.get(`http://localhost:3001/data/?q=${value}`)
+    this.setState({
+      list: res.data
+    })
+  }
+  // 删除
+  handleDelete = async (id) => {
+    await axios.delete(`http://localhost:3001/data/${id}`)
+    this.loadList()
+  }
+  // 加载列表
+  loadList = async () => {
+    const res = await axios.get('http://localhost:3001/data')
+    console.log(res)
+    this.setState({
+      list: res.data
+    })
+  }
+  componentDidMount () {
+    // 发送接口请求
+    this.loadList()
+  }
+  render () {
+    return (
+      <div className="container">
+        <div className="search-box">
+          <Search
+            placeholder="请输入关键词"
+            allowClear
+            enterButton="搜索"
+            size="large"
+            onChange={this.inputChange}
+            value={this.state.keyword}
+            onSearch={this.onSearch}
+          />
+        </div>
+        <Table bordered dataSource={this.state.list} rowKey= {(record) =>  record.id } columns={this.state.columns} pagination={false} />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Date: 2022-07-31 10:50:29
+ * @LastEditors: siwenfeng
+ * @LastEditTime: 2022-07-31 21:55:43
+ */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
