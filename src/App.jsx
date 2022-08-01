230a580d95a5c2939edb3d74de6c0a287fd17612
siwenfeng <287235779@qq.com>
2022-08-01 22:14:40 +0800

    完成实时更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
 // import { useState, useEffect } from "react";
 
 // import store from './store/index'
-import { addAction } from './store/actions'
+import { addAction } from "./store/actions";
 
+import { connect } from "react-redux";
+// 1.mapStateToProps方法：告诉React-Redux, 需要将store中保存的哪些数据映射到当前组件的props上
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+  };
+};
+// 2.mapDispatchToProps方法：告诉React-Redux, 需要将哪些派发的任务映射到当前组件的props上
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment() {
+      dispatch(addAction(1));
+    },
+  };
+};
 
-// console.log(store.getState())
-// console.log(store.dispatch(addAction(1)))
-// console.log(store.getState())
-
-
-
-
-function App() {
+function App(props) {
   return (
     <>
-    <button onClick={() => { store.dispatch(addAction(1)) }}>加一</button>
-    <div>{store.getState().count}</div>
+      {/* 3.通过props来使用redux中保存的数据 */}
+      <p>{props.count}</p>
+      <button
+        onClick={() => {
+          props.increment();
+        }}
+      >
+        递增
+      </button>
     </>
-  )
+  );
 }
-export default App
+// export default App
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,23 @@
 /*
- * @Description: 
+ * @Description:
  * @Date: 2022-07-31 10:50:29
  * @LastEditors: siwenfeng
- * @LastEditTime: 2022-08-01 22:10:53
+ * @LastEditTime: 2022-08-01 22:12:29
  */
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-// import { Provider } from "react-redux";
-// // import store from "./store";
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  {/* <Provider store={store}> */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  {/* </Provider> */}
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
-)
+);
